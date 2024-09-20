9fe8328c17ed74be2bfd4a0db2f274162e23966b
wilson101xx <wilsonadam15@gmail.com>
2024-09-20 23:39:04 +0100

    Update function to handle edge cases

--- venv/Lib/site-packages/semantic_kernel/connectors/memory/weaviate/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Microsoft. All rights reserved.
-
-from semantic_kernel.connectors.memory.weaviate.weaviate_memory_store import (
-    WeaviateMemoryStore,
-)
-from semantic_kernel.connectors.memory.weaviate.weaviate_settings import WeaviateSettings
-
-__all__ = ["WeaviateMemoryStore", "WeaviateSettings"]
